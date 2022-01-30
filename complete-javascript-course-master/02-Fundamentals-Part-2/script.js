387c6c3a71aedcc652fe77639e66385d5581a9b0
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-01-30 17:48:35 -0300

    challenge 4-2

--- complete-javascript-course-master/02-Fundamentals-Part-2/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/script.js
@@ -25,10 +25,26 @@ const calcAverage = (a, b, c) => (a + b + c) / 3;
 let dolphAverage = calcAverage(4, 5, 8000);
 let koalasAverage = calcAverage(4, 5, 600);
 
-console.log(dolphAverage);
-console.log(koalasAverage);
-
 function checkWinner(dolphAverage, koalasAverage) {
     return (dolphAverage > koalasAverage) ? "dolph Wins" : "koalas Wins";
 }
 console.log(checkWinner(dolphAverage, koalasAverage));
+
+const pokemon = ['pikachu', 'abra', 'ratata']
+
+
+function tip(bill) {
+
+    let calcTip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.2);
+
+    console.log(`The bill is ${bill}, the tip is ${calcTip} and the total is ${bill + calcTip}`)
+    return calcTip
+}
+
+const dados = [124, 555, 44];
+let total = [];
+total.push(tip(dados[0]) + dados[0]);
+total.push(tip(dados[1]) + dados[1]);
+total.push(tip(dados[2]) + dados[2]);
+
+console.log(total);
