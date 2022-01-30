cb1738e1d8ab3512d17ce1c1b04c22602383ec35
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-01-30 14:01:55 -0300

    before first challenge

--- complete-javascript-course-master/01-Fundamentals-Part-1/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/script.js
@@ -3,5 +3,10 @@
 let js = 'fun';
 let sum = 1 + 2 + 3 + 4 + 5 + 6;
 if (js == 'fun') alert('Javascript is ' + js + " " + sum);
-console.log(sum);
+//console.log(sum);
 
+let age = 32;
+age = 33;
+console.log(sum + age);
+verdade = (age > sum);
+console.log(typeof (verdade) == "boolean")
