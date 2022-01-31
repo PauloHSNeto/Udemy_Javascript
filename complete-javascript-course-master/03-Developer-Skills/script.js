343f495dc72b0d3180e2a210fe065c42b68242c5
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-01-31 07:44:34 -0300

    challenge completed

--- complete-javascript-course-master/03-Developer-Skills/script.js
+++ complete-javascript-course-master/03-Developer-Skills/script.js
@@ -1,4 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-let a = b;
+function printforecast(array) {
+  let answer = "";
+  for (let i = 0; i < array.length; i++) {
+    answer += ` ... ${array[i]}° in ${i + 1} days ... `;
+  }
+  console.log(answer);
+}
+
+printforecast([99, 25, 17, 21, 23]);
--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+function printforecast(array) {
+  let answer = "";
+  for (let i = 0; i < array.length; i++) {
+    answer += ` ...${array[i]}° in ${i} days... `;
+  }
+  console.log(answer);
+}
+
+printforecast([17, 21, 23]);
