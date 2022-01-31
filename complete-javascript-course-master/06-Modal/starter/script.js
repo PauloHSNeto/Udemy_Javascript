6c01c7c4bc5b1965b49da313f41aadfdc8934e7a
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-01-31 17:59:03 -0300

    project  2 complete

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButtom = document.querySelector('.close-modal');
+const openButtom = document.querySelectorAll('.show-modal');
+console.log(openButtom);
+
+for (let i = 0; i < openButtom.length; i++) {
+  openButtom[i].addEventListener('click', function () {
+    console.log('Butt-on');
+    modal.classList.remove('hidden');
+  });
+}
